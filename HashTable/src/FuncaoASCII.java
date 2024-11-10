255b5f938c7d4d5f192f9d9a7e91c083af13814f
Caroline Assis <caroolassissss@gmail.com>
2024-11-10 19:12:45 -0300

    arrumando as fucnoes

--- HashTable/src/FuncaoASCII.java
+++ HashTable/src/FuncaoASCII.java
@@ -9,14 +9,3 @@ public class FuncaoASCII extends HashTable {
         return sum % size;
     }
 }
-public class FuncaoASCII extends HashTable {
-
-    @Override
-    public int hash(String value) {
-        int sum = 0;
-        for (char c : value.toLowerCase().toCharArray()) {
-            sum += c - 'a' + 1;  // Converte 'a' a 'z' para valores de 1 a 26
-        }
-        return sum % size;
-    }
-}
