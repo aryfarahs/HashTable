afa69f495cb5eddfb0034b042311a0885851b4d0
babi <141651503+babitasz@users.noreply.github.com>
2024-11-12 15:12:40 -0300

    divando 	modified:   HashTable/src/FuncaoASCII.java 	modified:   HashTable/src/FuncaoNormal.java 	modified:   HashTable/src/Main.java 	deleted:    "HashTable/src/Relat\303\263rio Tabela Hash (1).pdf" 	renamed:    "HashTable/src/Relat\303\263rio Tabela Hash (2).pdf" -> "HashTable/src/Relat\303\263rio Tabela Hash (3).pdf"

--- HashTable/src/FuncaoNormal.java
+++ HashTable/src/FuncaoNormal.java
@@ -3,6 +3,14 @@ public class FuncaoNormal extends HashTable {
     @Override
     public int hash(String value) {
         int length = value.length();
-        return length % size;
+        int hash = length * 17;  // Multiplica o comprimento por um número primo
+
+        // Combinando os valores dos caracteres da string com o comprimento
+        for (int i = 0; i < value.length(); i++) {
+            hash = (hash * 31) + value.charAt(i);  // Número primo 31 para dispersão
+        }
+
+        // Aplica o módulo para garantir que o valor de hash caiba dentro do tamanho da tabela
+        return Math.abs(hash % size);
     }
 }
