b9e1ff5cc713fdd3fea2e39be3480226f407472d
babi <141651503+babitasz@users.noreply.github.com>
2024-11-12 15:13:54 -0300

    divas

--- HashTable/src/FuncaoNormal.java
+++ HashTable/src/FuncaoNormal.java
@@ -3,14 +3,10 @@ public class FuncaoNormal extends HashTable {
     @Override
     public int hash(String value) {
         int length = value.length();
-        int hash = length * 17;  // Multiplica o comprimento por um número primo
-
-        // Combinando os valores dos caracteres da string com o comprimento
+        int hash = length * 17; 
         for (int i = 0; i < value.length(); i++) {
-            hash = (hash * 31) + value.charAt(i);  // Número primo 31 para dispersão
+            hash = (hash * 31) + value.charAt(i);  
         }
-
-        // Aplica o módulo para garantir que o valor de hash caiba dentro do tamanho da tabela
         return Math.abs(hash % size);
     }
 }
